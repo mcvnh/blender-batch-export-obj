e1b0871fd26d197d68629760b9884d7c7a67fb5a
TAXSET <40140669+TAXSET@users.noreply.github.com>
2022-09-16 01:44:35 +0200

    Updating to Blender 3.2
    Adapting existing codebase to API changes

--- batchexport_cli.py
+++ batchexport_cli.py
@@ -3,20 +3,21 @@ import os
 
 
 def run():
-    # deselect all objects
-    bpy.ops.object.select_all(action='DESELECT')    
+	# Deselect all objects
+	for obj in bpy.context.selected_objects:
+		obj.select_set(False) 
 
-    objects = bpy.context.scene.objects 
-    for ob in objects:
-        objects.active = ob
-        ob.select = True
+    # Iterate over all objects and export them
+	objects = bpy.context.view_layer.objects
+	for ob in objects:
+		objects.active = ob
+		ob.select_set(True)
 
-        if ob.type == 'MESH':
-            bpy.ops.export_scene.obj(filepath=os.path.join(os.environ['OBJ_EXPORT_PATH'], ob.name + '.obj'), use_selection=True)
+		if ob.type == 'MESH':
+			bpy.ops.export_scene.obj(filepath=os.path.join(os.environ['OBJ_EXPORT_PATH'], ob.name + '.fbx'), use_selection=True)
 
-        ob.select = False
+		ob.select_set(False)
 
 
 if __name__ == "__main__":
-    run()
-
+	run()
